3c8df5eed35fdb0b2f71f9de71a4f1d49e8ad040
berkanozm <ozmberkan@gmail.com>
2024-02-13 14:30:26 +0300

    Src Kontrolü

--- src/pages/home/Footer.jsx
+++ src/pages/home/Footer.jsx
@@ -7,15 +7,15 @@ export default function Footer() {
   return (
     <div className='w-full h-screen  flex flex-col justify-end items-center '>
       <div className='bg-white h-[500px] w-full flex flex-row justify-start items-center px-12'>
-        <div className='w-[25%] h-full  px-4 py-4'>
+        <div className='w-[25%] h-full px-4 py-4 bg-green-500'>
           <h1 className='font-damion text-[50px]'>easycourse</h1>
           <div className='flex flex-col gap-y-4 '>
-            <img src={appStore} alt=""className='w-64 h-20 cursor-pointer' />
-            <img src={playStore} alt="" className='w-64 h-20 cursor-pointer'/>
-            <img src={huawei} alt="" className='w-64 h-20 cursor-pointer'/>
+            <img src={appStore} alt=""className=' cursor-pointer w-44 h-12' />
+            <img src={playStore} alt="" className='cursor-pointer w-44 h-12'/>
+            <img src={huawei} alt="" className='cursor-pointer w-44 h-12'/>
           </div>
         </div>
-        <div className='w-[25%]  h-full px-4 py-24'>
+        <div className='w-[25%]  h-full px-4 py-24 bg-yellow-500'>
           <ul className='flex flex-col gap-y-8 justify-center items-start'>
             <li className='text-xl'>Hakkımızda</li>
             <li className='text-xl'>İletişim</li>
@@ -24,7 +24,7 @@ export default function Footer() {
             <li className='text-xl'>Teknoloji Eğitimleri</li>
           </ul>
         </div>
-        <div className='w-[25%]  h-full px-4 py-24'>
+        <div className='w-[25%]  h-full px-4 py-24 bg-yellow-500'>
         <ul className='flex flex-col gap-y-8 justify-center items-start'>
             <li className='text-xl'>Sıkça Sorulan Sorular</li>
             <li className='text-xl'>Kişisel Verilerin Korunması</li>
@@ -33,7 +33,7 @@ export default function Footer() {
             <li className='text-xl'>Çerez Politikası</li>
           </ul>
         </div>
-        <div className='w-[25%] h-full px-4 py-24 flex flex-col '>
+        <div className='w-[25%] h-full px-4 py-24 flex flex-col bg-yellow-500 '>
         <button className='border-[2px] border-primary-color text-primary-color h-12 rounded-md mt-5 font-montserrat w-full' >Avantajlar</button>
         <button className='border-[2px] border-primary-color text-primary-color h-12 rounded-md mt-5 font-montserrat w-full' >Avantajlar</button>
         <button className='border-[2px] border-primary-color text-primary-color h-12 rounded-md mt-5 font-montserrat w-full' >Avantajlar</button>
