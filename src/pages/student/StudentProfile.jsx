da3763b0208922537fe570365b48e54c60cb99bf
berkanozm <ozmberkan@gmail.com>
2024-02-18 20:24:18 +0300

    button fix

--- src/pages/student/StudentProfile.jsx
+++ src/pages/student/StudentProfile.jsx
@@ -20,15 +20,13 @@ export default function StudentProfile() {
               <p className='text-[#A8AEAF] text-xl'><span className='text-primary-color font-montserrat text-md font-semibold'>s</span> - @ozmberkan</p>
               <img src={studentImg} alt="" className='rounded-xl w-full h-[350px] object-cover' />
               <div className='grid grid-cols-2 grid-rows-2 gap-x-4 gap-y-4 items-center py-4'>
-                <button className='bg-primary-color px-4 py-4 rounded-xl text-center text-white font-montserrat'>Şifre Değiştir</button>
-                <button className='bg-primary-color px-4 py-4 rounded-xl text-center text-white font-montserrat'>Bildirim Ayarları</button>
-                <button className='bg-primary-color px-4 py-4 rounded-xl text-center text-white font-montserrat'>E-Posta Değiştir</button>
-                <div className='relative '>
-                  <input type="text" name="teacherCode" id="teacherCode" className='border w-full border-primary-color py-4 text-sm rounded-xl text-center text-primary-color font-montserrat' placeholder='Eğitmen Kodu' />
-                  <IoMdArrowDroprightCircle className='absolute right-3 top-[20px] flex items-center cursor-pointer' size={15} />
-                </div>
+                <button className='bg-primary-color px-4 py-4 w-full h-[50px] rounded-xl text-center text-white font-montserrat flex justify-center items-center'>Şifre Değiştir</button>
+                <button className='bg-primary-color px-4 py-4 w-full h-[50px] rounded-xl text-center text-white font-montserrat flex justify-center items-center'>Bildirim Ayarları</button>
+                <button className='bg-primary-color px-4 py-4 w-full h-[50px] rounded-xl text-center text-white font-montserrat flex justify-center items-center'>E-Posta Değiştir</button>
+                <input type="text" name="teacherCode" id="teacherCode" className='border w-full border-primary-color py-4 text-sm rounded-xl text-center text-primary-color font-montserrat flex justify-center items-center' placeholder='Eğitmen Kodu' />
               </div>
-              <button className='w-full border border-primary-color px-4 py-4 rounded-md font-montserrat mt-4'>Profil Resmi Ekle/Değiştir</button>
+              <button className='w-full border border-primary-color px-4 py-4 rounded-md font-montserrat'>Eğitmen Kodunu Gönder</button>
+              <button className='w-full bg-primary-color text-white px-4 py-4 rounded-md font-montserrat'>Profil Resmi Ekle/Değiştir</button>
             </div>
             <div className='bg-white w-full h-full rounded-lg shadow-md py-6 px-6'>
               <h1 className='text-primary-color font-montserrat text-[35px] font-semibold' >Ders talebi oluştur!</h1>
@@ -47,7 +45,7 @@ export default function StudentProfile() {
                   <option value="one">Ferhat TOKER</option>
                   <option value="one">Buket YILDIRIM</option>
                 </select>
-                <input type="time" className='w-full border-2 border-primary-color px-4 py-4 rounded-lg ' />
+                <input type="time" className='w-full border-2 border-primary-color px-4 py-4 rounded-l  ' />
                 <button className='bg-primary-color px-4 py-4 rounded-xl text-center text-white font-montserrat mb-5'>Ders Talebi Oluştur</button>
               </div>
               <div className=' w-full rounded-md h-[350px] flex flex-col gap-y-4 py-3'>
